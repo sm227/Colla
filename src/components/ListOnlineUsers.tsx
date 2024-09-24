18204111bccf77ebe5cabdfbc1424f4aa5e033a3
Seungmin Ham <hamsm3184@naver.com>
2024-09-24 20:01:33 +0900

    user list

--- src/components/ListOnlineUsers.tsx
+++ src/components/ListOnlineUsers.tsx
@@ -1,16 +1,20 @@
 "use client";
 import { useUser } from "@clerk/nextjs";
 import { useSocket } from "../../context/SocketContext";
+import Avatar from "./Avatar";
 
 const ListOnlineUsers = () => {
   const { user } = useUser();
   const { onlineUsers } = useSocket();
 
   return (
-    <div>
+    <div className="text-black flex mt-2 gap-4 border-b border-b-primary/10 w-full">
       {onlineUsers &&
         onlineUsers.map((onlineUser) => {
-          return <div></div>;
+          return <div key={onlineUser.userId} className="flex flex-col items-center gap-1 cursor-pointer">
+            <Avatar src={onlineUser.profile.imageUrl}/>
+            <div className="text-sm">{onlineUser.profile.fullName?.split(' ')[0]}</div>
+          </div>;
         })}
     </div>
   );
